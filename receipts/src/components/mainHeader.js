be699d84d07a8d9a9c774b80096acc637bc18a8c
Kamui Shirou <x_kamui@msn.com>
2022-10-25 06:13:47 +0200

    Ajout de l'entête en mode statique

---
+++ receipts/src/components/mainHeader.js
@@ -0,0 +1,15 @@
+import '../assets/styles/mainHeader.css'
+
+function MainHeader(){
+
+    return (
+        <header>
+            <div className="header-buttn"><button>New</button></div>
+            <div className="header-title"><h1>Manage your receipts</h1></div>
+            <div className="header-total">Total : <span className="price-total">3.255,00 €</span></div>
+        </header>
+    );
+
+}
+
+export default MainHeader;
